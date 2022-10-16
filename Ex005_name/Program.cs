b3c31ea479b4558e28842e2ffff2ec0442a6acb6
mikalayDZ <sekta43@gmail.com>
2022-10-16 19:47:19 +0300

    add ex005_name

---
+++ Ex005_name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юля") 
+{
+    Console.WriteLine("Так это же ЮЛЯ");
+}
+else
+{
+    Console.Write("Ну привет, ");
+    Console.WriteLine(username);
+}
